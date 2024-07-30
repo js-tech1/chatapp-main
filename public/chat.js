d017cbe51a6b87e06880bc311b7e9293f1f31215
js-tech1 <59276982+js-tech1@users.noreply.github.com>
2024-07-30 20:36:10 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const usersjoin = document.querySelector(".users-join");
 
   const url = new URL(window.location.href);
-  const socket = io.connect(url.href, { secure: true });
+  const socket = io.connect(url.href, { path: '/socket.io', secure: true });
   // console.log(url);
 
   fetch("/get-username-roomname")
