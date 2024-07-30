2d9691f18fb9b0e21e12690474bedabf148a1155
js-tech1 <59276982+js-tech1@users.noreply.github.com>
2024-07-30 19:03:21 +0530

    aaa

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require('express-session');
-const socket = require("socket.io");
+const socketIo = require("socket.io");
 const crypto = require('crypto');
 const path = require('path');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'utils')));
-
-var port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.render("index");
+const server = require("http").createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../views'));
+app.set("view engine", "ejs");
+
 const secretKey = crypto.randomBytes(32).toString('hex');
 app.use(session({
   secret: secretKey,
@@ -26,43 +26,35 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-
-const server = app.listen(port, '0.0.0.0', () => {
-  console.log(`Server Running on 3000`);
+app.get("/", (req, res) => {
+  res.render("index");
 });
-const io = socket(server);
-require("./utils/socket")(io);
 
-// app.post("/room", (req, res) => {
-//   roomname = req.body.roomname;
-//   username = req.body.username;
-//   const uniqueIdentifier = Math.floor(Math.random() * 10000);
-//   res.redirect(`/room?username=${username+uniqueIdentifier}&roomname=${roomname}`);
-// });
 app.post('/room', (req, res) => {
   const { username, roomname } = req.body;
-
-  // Store the username and roomname in the session
   req.session.username = username + Math.floor(Math.random() * 10000);
   req.session.roomname = roomname;
-
-  // Redirect to the room without exposing parameters in the URL
   res.redirect('/room');
 });
+
 app.get("/get-username-roomname", (req, res) => {
   const { username, roomname } = req.session;
-  console.log("get", username); // Assuming you stored them in the session
   res.json({ username, roomname });
 });
+
 app.get('/room', (req, res) => {
-  // Access the stored username and roomname from the session
   const { username, roomname } = req.session;
-  console.log(username, roomname);
-  // Render your room template with the username and roomname
   res.render('room', { username, roomname });
 });
-// app.get("/room", (req, res) => {
-//   res.render("room");
-// });
 
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
+require("../utils/socket")(io);
+
+module.exports = app;
+module.exports = server;
